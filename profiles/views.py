2e42cebe97507799039a120a47db5d17e5cf6275
kronos <kronos3l1t3@gmail.com>
2022-05-30 19:14:27 -0400

    [#7]-Add views

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from rest_framework import viewsets, filters
+from rest_framework import status
+
+from .serializers import SerializerFriend, SerializerProfile
+from .models import Friend, Profile
+
+
+class ProfileView(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = SerializerProfile
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('id', )
+    search_fields = ['id']
+    ordering_fields = '__all__'
+
+
+class FriendView(viewsets.ModelViewSet):
+    queryset = Friend.objects.all()
+    serializer_class = SerializerFriend
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('id', 'profile1__first_name', 'profile2__first_name')
+    search_fields = ['id']
+    ordering_fields = '__all__'
