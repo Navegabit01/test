b38f6fd339647c64d96ea4670f783f83e04a592b
kronos <kronos3l1t3@gmail.com>
2022-05-30 18:59:42 -0400

    [#3]-Add settings

--- test_dev/settings.py
+++ test_dev/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#APPS LOCAL VARS CONFIG
+# APPS LOCAL VARS CONFIG
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
@@ -146,7 +146,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/upload')
 
 CACHES = {
