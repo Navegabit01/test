b3f84a6585d3364fcda45edf2abdea36572f5f36
kronos <kronos3l1t3@gmail.com>
2022-05-30 19:05:53 -0400

    [#6]-Add serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,3 +1,14 @@
 from rest_framework import serializers
-from .models import Profile, Friends
+from .models import Profile, Friend
 
+
+class SerializerFriend(serializers.ModelSerializer):
+    class Meta:
+        model = Friend
+        fields = '__all__'
+
+
+class SerializerProfile(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
